f53c6d953d8572751b9dfa82f0f0c92a80cf05f1
DL23727272 <113783762+DL23727272@users.noreply.github.com>
2024-04-10 13:58:43 +0800

    fixed navbar link

--- resources/views/components/navigation/navbar.blade.php
+++ resources/views/components/navigation/navbar.blade.php
@@ -22,7 +22,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link  " href="#content">
+            <a class="nav-link  " href="/home#content">
               Product
             </a>
           </li>
