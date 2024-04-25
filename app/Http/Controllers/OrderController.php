8457f959c9c5a20a61f4e273b5d918b5e093d35f
DL23727272 <113783762+DL23727272@users.noreply.github.com>
2024-04-25 21:46:33 +0800

    edited OrderController

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,18 +7,16 @@ use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
-    // Function to place an order
     public function placeOrder(Request $request)
     {
         $customerID = $request->session()->get('customerID');
         $cartItems = json_decode($request->session()->get('cartItems'), true) ?? [];
 
         if ($customerID && count($cartItems) > 0) {
-            // Start a database transaction
+
             DB::beginTransaction();
 
             try {
-                // Create a new order
                 $order = DB::table('order_table')->insertGetId([
                     'customerID' => $customerID,
                     'totalPrice' => $this->calculateTotalPrice($cartItems),
@@ -27,7 +25,6 @@ class OrderController extends Controller
 
                 ]);
 
-                // Save order items
                 $orderItems = [];
                 foreach ($cartItems as $item) {
                     $orderItems[] = [
@@ -40,30 +37,24 @@ class OrderController extends Controller
                 }
                 DB::table('order_items_table')->insert($orderItems);
 
-                // Commit the transaction
                 DB::commit();
 
-                // Clear the cart after placing the order
                 $request->session()->forget('cartItems');
 
-                // Order placed successfully
                 return response()->json([
                     'status' => 'success',
                     'message' => 'Order placed successfully!',
                     'orderID' => $order
                 ]);
             } catch (\Exception $e) {
-                // Something went wrong, rollback the transaction
                 DB::rollBack();
 
-                // Failed to place the order
                 return response()->json([
                     'status' => 'error',
                     'message' => 'Failed to place order. Please try again later.'
                 ]);
             }
         } else {
-            // Show an error message if customerID is missing or cart is empty
             return response()->json([
                 'status' => 'error',
                 'message' => 'Please Login or Cart is empty!'
@@ -71,7 +62,6 @@ class OrderController extends Controller
         }
     }
 
-    // Function to calculate total price of items in the cart
     private function calculateTotalPrice($cartItems)
     {
         $totalPrice = 0;
